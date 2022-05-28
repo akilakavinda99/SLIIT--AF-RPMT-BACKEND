ff3ac5ac9ceca20a161040ff129b41d0675a100f
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-05-28 22:20:41 +0530

    admin delete submission

--- RMT/BACKEND/routes/submission.js
+++ RMT/BACKEND/routes/submission.js
@@ -22,13 +22,61 @@ router.route('/create').post((req, res) => {
 
 // Get all submisions
 router.route('/').get((req, res) => {
-    console.log("awawa");
     Submission.find()
         .then(submissions => {
             res.status(200).send({ status: "Data successfully fetched.", submissions: submissions })
         })
         .catch(err => {
-            res.status(500).send({error: "Error with fetchin data."})
+            res.status(500).send({ error: "Error with fetchin data." })
+        })
+})
+
+
+// Get specific submission
+router.route('/:submissionId').get((req, res) => {
+    const submissionId = req.params.submissionId
+
+    Submission.findById(submissionId)
+        .then(submission => {
+            res.status(200).send({ status: "Data successfully fetched.", submission })
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(500).send({ error: err.message })
+        })
+})
+
+
+// Get specific submission
+router.route('/update/:submissionId').put((req, res) => {
+    const submissionId = req.params.submissionId
+    const newData = req.body
+
+    Submission.findByIdAndUpdate(submissionId, newData)
+        .then(() => {
+            res.status(200).send({ status: "Submission updated." })
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(500).send({ error: "Error with updating data." })
+        })
+})
+
+
+// Delete submission
+router.route('/delete/:submissionId').delete((req, res) => {
+    const submissionId = req.params.submissionId
+    // console.log(submissionId);
+
+    Submission.findByIdAndDelete(submissionId)
+        .then(() => {
+            res.status(200)
+                .send({ status: "Submission successfully deleted." })
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(500)
+                .send({ error: "Error with deleting submission." })
         })
 })
 
