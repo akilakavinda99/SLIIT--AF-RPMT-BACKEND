3c124c8601ac2480e1bf20dca17bf065a2572d16
Akila Kavinda <akilakavinda909@gmail.com>
2022-05-29 00:44:10 +0530

    error fix

--- RMT/BACKEND/routes/submission.js
+++ RMT/BACKEND/routes/submission.js
@@ -18,65 +18,18 @@ router.route("/create").post((req, res) => {
 });
 
 // Get all submisions
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   Submission.find()
-    .then(submissions => {
-      res.status(200).send({ status: "Data successfully fetched.", submissions: submissions })
+    .then((submissions) => {
+      res.status(200).send({
+        status: "Data successfully fetched.",
+        submissions: submissions,
+      });
     })
-    .catch(err => {
-      res.status(500).send({ error: "Error with fetchin data." })
-    })
-})
-
-
-// Get specific submission
-router.route('/:submissionId').get((req, res) => {
-  const submissionId = req.params.submissionId
-
-  Submission.findById(submissionId)
-    .then(submission => {
-      res.status(200).send({ status: "Data successfully fetched.", submission })
-    })
-    .catch(err => {
-      console.log(err.message);
-      res.status(500).send({ error: err.message })
-    })
-})
-
-
-// Get specific submission
-router.route('/update/:submissionId').put((req, res) => {
-  const submissionId = req.params.submissionId
-  const newData = req.body
-
-  Submission.findByIdAndUpdate(submissionId, newData)
-    .then(() => {
-      res.status(200).send({ status: "Submission updated." })
-    })
-    .catch(err => {
-      console.log(err.message);
-      res.status(500).send({ error: "Error with updating data." })
-    })
-})
-
-
-// Delete submission
-router.route('/delete/:submissionId').delete((req, res) => {
-  const submissionId = req.params.submissionId
-  // console.log(submissionId);
-
-  Submission.findByIdAndDelete(submissionId)
-    .then(() => {
-      res.status(200)
-        .send({ status: "Submission successfully deleted." })
-    })
-    .catch(err => {
-      console.log(err.message);
-      res.status(500)
-        .send({ error: "Error with deleting submission." })
-    })
-})
-
+    .catch((err) => {
+      res.status(500).send({ error: "Error with fetchin data." });
+    });
+});
 
 // Get all  available submisions
 router.route("/availableSubmissions").get((req, res) => {
@@ -92,6 +45,52 @@ router.route("/availableSubmissions").get((req, res) => {
     })
     .catch((err) => {
       res.status(500).send({ error: "Error with fetchin data." });
+    });
+});
+
+// Get specific submission
+router.route("/:submissionId").get((req, res) => {
+  const submissionId = req.params.submissionId;
+
+  Submission.findById(submissionId)
+    .then((submission) => {
+      res
+        .status(200)
+        .send({ status: "Data successfully fetched.", submission });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({ error: err.message });
+    });
+});
+
+// Get specific submission
+router.route("/update/:submissionId").put((req, res) => {
+  const submissionId = req.params.submissionId;
+  const newData = req.body;
+
+  Submission.findByIdAndUpdate(submissionId, newData)
+    .then(() => {
+      res.status(200).send({ status: "Submission updated." });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({ error: "Error with updating data." });
+    });
+});
+
+// Delete submission
+router.route("/delete/:submissionId").delete((req, res) => {
+  const submissionId = req.params.submissionId;
+  // console.log(submissionId);
+
+  Submission.findByIdAndDelete(submissionId)
+    .then(() => {
+      res.status(200).send({ status: "Submission successfully deleted." });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send({ error: "Error with deleting submission." });
     });
 });
 
