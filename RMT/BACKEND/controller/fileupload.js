6238298e834e294fd23a26e1bb7294826bd52af5
Yasith Chandula <76646376+yasithchandula@users.noreply.github.com>
2022-06-04 11:11:55 +0530

    fileupload-update

---
+++ RMT/BACKEND/controller/fileupload.js
@@ -0,0 +1,29 @@
+// import Item from "../models/item.js";
+const Item=require("../models/documentup");
+    const getItems = async(req,res)=>{
+    console.log('get items')
+    try {
+        const item =await Item.find()
+       
+        res.status(200).json(item);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+
+    }
+    
+}
+
+
+
+const createItem = async(req,res)=>{
+    console.log('createitem',req.body)
+    const item = new Item(req.body);
+    try {
+        await item.save();
+        res.status(201).json(item);
+    } catch (error) {
+        
+    }
+}
+
+export default (getItems,createItem);
